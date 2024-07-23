0b4c8aa9e9c913f1ffc053bc024064414cf9f7d1
RaresIustin <ionita.rares78@yahoo.com>
2024-07-23 09:26:17 +0300

    Finished???

--- GuessTheWord/GuessTheWord.cpp
+++ GuessTheWord/GuessTheWord.cpp
@@ -1,8 +1,11 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <time.h>
 #include <string>
 #include <map>
 #include <fstream>
 #include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 
@@ -11,7 +14,7 @@ bool isGuessed;
 
 int mistakes = 3;
 
-string word = "dark";
+string word[10] = {"copil" , "munte", "floare", "culoare", "carte", "vis", "fruct", "apa", "arbore", "stea"};
 string wordchecker;
 
 void resetConsole() {
@@ -22,39 +25,50 @@ void resetConsole() {
 #endif
 }
 
-int main() {
+void SetColor(int textColor, int bgColor) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, (bgColor << 4) | textColor);
+}
 
+int main() {
+	SetColor(13, 0);
+
+	srand((unsigned)time(NULL));
+	int random = rand() % 10;
+	cout << random;
+
+	/**/
 	wordchecker = "|";
-	for (int i = 0; i < word.length(); i++)
+	for (int i = 0; i < word[random].length(); i++)
 		wordchecker += "*";
 	wordchecker += "|";
 	cout << wordchecker;
 
-
-	/**/while (gameLoop) {
+	while (gameLoop) {
+		
 		//Resetare Consola
 		resetConsole();
 		///////
 		cout << wordchecker << endl;
 		
 		char guess;
-		cout << "Introduceți o literă: ";
+		cout << "Introduceti o litera: " << endl;
+		cout << "Mai aveti " << mistakes << " greseli";
 		guess = _getch();
 
 		bool charFound = false;
-		for (int i = 0; i < word.length(); i++)
-			if (word[i] == guess) {
+		for (int i = 0; i < word[random].length(); i++)
+			if (word[random][i] == guess) {
 				wordchecker[i + 1] = guess;
 				charFound = true;
 			}
 
 		if (!charFound) {
 			mistakes--;
-			cout << "Mai aveti " << mistakes << "greseli";
 		}
 
 		isGuessed = true;
-		for (int i = 1; i <= word.length(); i++)
+		for (int i = 1; i <= word[random].length(); i++)
 			if (wordchecker[i] == '*') {
 				isGuessed = false;
 				break;
@@ -64,14 +78,14 @@ int main() {
 			gameLoop = false;
 			resetConsole();
 			cout << wordchecker << endl;
-			cout << "Ai câștigat! Cuvântul era: " << word << endl;
+			cout << "Ai castigat! Cuvantul era: " << word[random] << endl;
 		}
 
 		if (mistakes == 0) {
 			gameLoop = false;
 			resetConsole();
 			cout << wordchecker << endl;
-			cout << "Ai pierdut! Cuvântul era: " << word << endl;
+			cout << "Ai pierdut! Cuvantul era: " << word[random] << endl;
 		}
 
 	}
